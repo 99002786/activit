bee915a96a225916a1ecce4fee484cdd2a12f063
99002786 <72496123+99002786@users.noreply.github.com>
2020-10-07 23:43:34 +0530

    Add files via upload

---
+++ calci-main/calci-main/function.c
@@ -0,0 +1,22 @@
+#include<function.h>
+
+int add(int x,int y){
+    int z = x + y;
+    return z;
+}   
+
+int sub(int a,int b){
+    int c = a - b;
+    return  c;
+}   
+
+int mul(int d,int e)
+{
+    int f = d * e;
+    return f;
+}
+
+int Div(int g,int h){
+    int i = g / h;
+    return i;
+}
---
+++ calci-main/calci-main/function.h
@@ -0,0 +1,9 @@
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+
+#include <stdio.h>
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int Div(int, int);
+#endif
