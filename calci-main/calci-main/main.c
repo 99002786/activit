eb1ac946173c4cb72dd0b1fd096b1a3e595970ee
99002786 <72496123+99002786@users.noreply.github.com>
2020-10-08 12:58:36 +0530

    Update main.c

--- calci-main/calci-main/main.c
+++ calci-main/calci-main/main.c
@@ -4,8 +4,9 @@ int main()
 {
 	
 int num1, num2, choice;
+int factin,factout,primein,primeout;
 
-printf("[1] Add\v[2] Subtract\v[3] Multiply\v[4] Divide");
+printf("[1] Add\v[2] Subtract\v[3] Multiply\v[4] Divide\v[5] Factorial\v[6] Prime Number");
 scanf("%d", &choice);
 
 switch(choice){
@@ -38,6 +39,27 @@ switch(choice){
         scanf("%d", &num2);
         printf("%d", Div(num1,num2));
         break;
+    case 5:
+            printf("\nEnter the number to get factorial\n");
+            scanf("%d",&factin);
+            factout = factorial_func(factout);
+            printf("The factorial is %d",factout);
+            break;
+
+     case 6:
+            printf("\nEnter the number to check whether its prime number or not.\n");
+            scanf("%d",&primein);
+            primeout = prime_func(primeout);
+            if(primeout==1)
+            {
+                printf("\nIt's a prime number\n");
+            }
+            else
+            {
+                printf("\nIt's not a prime number");
+            }
+            break;
+       
     default:
         printf("That is not a valid choice.");
         break;
